beee06af543d25b1fc9d1c0442d490b35a5cf147
Etienne <etienneleblan@gmail.com>
2021-04-11 10:04:10 +0200

    deploy

---
+++ precache-manifest.412544f747d98728f2a88e5546eff012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73808d94f7865a41b33c",
+    "url": "/thoughts/css/app.0630f851.css"
+  },
+  {
+    "revision": "23a8f4d4e43f3e7f93005c5dfdd5db52",
+    "url": "/thoughts/index.html"
+  },
+  {
+    "revision": "5885ec77d5b613a421cb",
+    "url": "/thoughts/js/about.e0c3080f.js"
+  },
+  {
+    "revision": "73808d94f7865a41b33c",
+    "url": "/thoughts/js/app.dc144ea6.js"
+  },
+  {
+    "revision": "5ba01cda877a01131789",
+    "url": "/thoughts/js/chunk-vendors.768a0696.js"
+  },
+  {
+    "revision": "16a40ffccc66353bb662b00a4e7ddaa2",
+    "url": "/thoughts/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/thoughts/robots.txt"
+  }
+]);
